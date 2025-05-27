b0372428b323eccf95ed150466622484abc6db9e
Marianna Chairez <mari@Mariannas-MacBook-Air.local>
2025-05-27 13:13:11 -0700

    radio fixed

--- capstone/script.js
+++ capstone/script.js
@@ -1,19 +1,17 @@
 (function(){
-    'use strict'
+    'use strict';
     console.log('reading JS');
 
     const actionArea = document.querySelector('#actions');
+    const game = document.querySelector('#game');
     const selectSound = new Audio('audio/radio.mp3');
-    
-
-    
-
-    
-
 
     function radioStart() {
         const buttonClickSound = new Audio('audio/radio.mp3');
+        buttonClickSound.play();
         game.innerHTML = `<p>radio start</p>`; 
     }
+
+    actionArea.addEventListener('click', radioStart);
 
 })();
