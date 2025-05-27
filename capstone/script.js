ac96661e8a32204fc921bddddbd2dcf90547e123
Marianna Chairez <mari@Mariannas-MacBook-Air.local>
2025-05-27 12:30:26 -0700

    cap

---
+++ capstone/script.js
@@ -0,0 +1,19 @@
+(function(){
+    'use strict'
+    console.log('reading JS');
+
+    const actionArea = document.querySelector('#actions');
+    const selectSound = new Audio('audio/beep.mp3');
+    
+
+    
+
+    
+
+
+    function radioStart() {
+        const buttonClickSound = new Audio('audio/beep.mp3');
+        game.innerHTML = `<p>radio start</p>`; 
+    }
+
+})();
