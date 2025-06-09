beb0401cf062cc925e552d237b2a517f2a5971bd
mchairez <mchairez@ucdavis.edu>
2025-06-09 14:59:01 -0700

    updated proj

--- capstone/script.js
+++ capstone/script.js
@@ -1,17 +1,51 @@
-(function(){
-    'use strict';
-    console.log('reading JS');
+(function () {
+    const backgrounds = [
+    {
+        bg: 'url("background/back1.svg")',
+        image: 'radios/frame1.png'
+    },
+    {
+        bg: 'url("background/back2.svg")',
+        image: 'radios/frame2.png'
+    },
+    {
+        bg: 'url("background/back3.svg")',
+        image: 'radios/frame3.png'
+    }
+    ];
 
-    const actionArea = document.querySelector('#actions');
-    const game = document.querySelector('#game');
-    const selectSound = new Audio('audio/radio.mp3');
+    let currentIndex = 0;
+    const bgDiv = document.getElementById('background');
+    const overlayImage = document.getElementById('overlayImage');
+    const nextBtn = document.getElementById('nextBtn');
+    const showImageBtn = document.getElementById('showImageBtn');
 
-    function radioStart() {
-        const buttonClickSound = new Audio('audio/radio.mp3');
-        buttonClickSound.play();
-        game.innerHTML = `<p>radio start</p>`; 
+    function setBackground(index) {
+    // Fade out background
+    bgDiv.style.opacity = 0;
+
+    // Reset image visibility
+    overlayImage.classList.remove('visible');
+
+    setTimeout(() => {
+        // Set background
+        bgDiv.style.backgroundImage = backgrounds[index].bg;
+        bgDiv.style.opacity = 1;
+
+        // Set the image source for this background
+        overlayImage.src = backgrounds[index].image;
+    }, 400);
     }
 
-    actionArea.addEventListener('click', radioStart);
+    // Initialize
+    setBackground(currentIndex);
 
+    nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % backgrounds.length;
+    setBackground(currentIndex);
+    });
+
+    showImageBtn.addEventListener('click', () => {
+    overlayImage.classList.toggle('visible');
+    });
 })();
